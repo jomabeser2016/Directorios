ec4fdc9b0ce0c2860e9ad66edea8c40ebdff2489
jomabeser2016 <manu819@hotmail.com>
2016-08-31 04:04:54 -0500

    Add files via upload

---
+++ ConsultaPersona.cs
@@ -0,0 +1,21 @@
+﻿using SAT.SIAT.Model.GA.Persona;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EjemploMVC.Models 
+{
+    public class ConsultaPersona
+    {        
+        [DisplayName("Nombre:")]        
+        public string vNomPer { get; set; }
+
+        [DisplayName("Sexo:")]
+        public SelectList vSexo { get; set; }
+    }
+}
